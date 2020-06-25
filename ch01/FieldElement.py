2d396c3f2bf07b6bc13adc087ce19dc3a36ba491
calofmijuck <calofmijuck@snu.ac.kr>
2020-06-25 22:10:27 +0900

    Element in a finite field.

---
+++ ch01/FieldElement.py
@@ -0,0 +1,52 @@
+# Implementation of an element in a finite field
+
+class FieldElement:
+    def __init__(self, num, prime):
+        if num >= prime or num < 0:
+            error = "Num {} not in field range 0 to {}".format(num, prime - 1)
+            raise ValueError(error)
+        self.num = num
+        self.prime = prime
+    
+    def __repr__(self):
+        return 'F_{}({})'.format(self.prime, self.num)
+    
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.num == other.num and self.prime == other.prime
+
+    def __ne__(self, other):
+        return not(self == other)
+
+    def __add__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("Cannot add two numbers in different fields")
+        num = (self.num + other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __sub__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("Cannot subtract two numbers in different fields")
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __mul__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("Cannot multiply two numbers in different fields")
+        num = (self.num * other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __pow__(self, exp):
+        n = exp % (self.prime - 1)
+        num = pow(self.num, n, self.prime)
+        return self.__class__(num, self.prime)
+
+    def __truediv__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("Cannot divide two numbers in different fields")
+        num = self.num * other.inv()
+        return self.__class__(num, self.prime)
+    
+    def inv(self):
+        return self.__class__(pow(self.num, self.prime - 2, self.prime), self.prime)
